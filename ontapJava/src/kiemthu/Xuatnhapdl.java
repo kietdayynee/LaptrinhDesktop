17474625159aed85031bb26f9b265721e254380a
kietdayne <kietlehuu@gmail.com>
2023-12-01 09:36:18 +0700

    update

--- ontapJava/src/kiemthu/Xuatnhapdl.java
+++ ontapJava/src/kiemthu/Xuatnhapdl.java
@@ -1,10 +1,16 @@
 package kiemthu;
 
-public class xuatnhapdl {
+import java.util.Scanner;
+
+public class Xuatnhapdl {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello world");
+		System.out.println("Username: ");
+		Scanner sc = new Scanner(System.in);
+		String u = sc.nextLine() ;
+		System.out.println("Hello "+u);
 	}
 
 }
